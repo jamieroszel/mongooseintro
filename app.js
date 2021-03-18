7851d0ce3d47735186f6c321f064dd716b73366c
jamieroszel <jamie.s.roszel@gmail.com>
2021-03-17 20:03:47 -0400

    Tweet.find specific tweet

--- app.js
+++ app.js
@@ -31,11 +31,11 @@ db.on('error', (error) => console.log(error))
 // Time to make tweets! 
 
 // Create an object representing a new Tweet
-const myFirstTweet = {
-    title: 'Deep Thoughts',
-    body: 'Friends, I have been navel-gazing',
-    author: 'Karolin'
-}
+// const myFirstTweet = {
+//     title: 'Deep Thoughts',
+//     body: 'Friends, I have been navel-gazing',
+//     author: 'Karolin'
+// }
 
 // Create the Tweet
 // Tweet.create(myFirstTweet, (error, tweet) => {
@@ -49,66 +49,73 @@ const myFirstTweet = {
 
 // Making Lots of Tweets!
 
-const manyTweets = [
-    {
-      title: 'Deep Thoughts',
-      body: 'Friends, I have been navel-gazing',
-      author: 'Karolin'
-    },
-    {
-      title: 'Sage Advice',
-      body: 'Friends, I am vegan and so should you',
-      author: 'Karolin',
-      likes: 20
-    },
-    {
-      title: 'Whole Reality',
-      body: 'I shall deny friendship to anyone who does not exclusively shop at Whole Foods',
-      author: 'Karolin',
-      likes: 40
-    },
-    {
-      title: 'Organic',
-      body: 'Friends, I have spent $2300 to be one of the first people to own an organic smartphone',
-      author: 'Karolin',
-      likes: 162
-    },
-    {
-      title: 'Confusion',
-      body: 'Friends, why do you just respond with the word `dislike`? Surely you mean to click the like button?',
-          author: 'Karolin',
-      likes: -100
-    },
-    {
-      title: 'Vespa',
-      body: 'Friends, my Vespa has been upgraded to run on old french fry oil. Its top speed is now 11 mph',
-      author: 'Karolin',
-      likes: 2
-    },
-    {
-      title: 'Licensed',
-      body: 'Friends, I am now officially licensed to teach yogalates. Like this to get 10% off a private lesson',
-      author: 'Karolin',
-      likes: 3
-    },
-    {
-      title: 'Water',
-      body: 'Friends, I have been collecting rain water so I can indulge in locally sourced raw water. Ask me how',
-      author: 'Karolin',
-    },
-  ];
+// const manyTweets = [
+//     {
+//       title: 'Deep Thoughts',
+//       body: 'Friends, I have been navel-gazing',
+//       author: 'Karolin'
+//     },
+//     {
+//       title: 'Sage Advice',
+//       body: 'Friends, I am vegan and so should you',
+//       author: 'Karolin',
+//       likes: 20
+//     },
+//     {
+//       title: 'Whole Reality',
+//       body: 'I shall deny friendship to anyone who does not exclusively shop at Whole Foods',
+//       author: 'Karolin',
+//       likes: 40
+//     },
+//     {
+//       title: 'Organic',
+//       body: 'Friends, I have spent $2300 to be one of the first people to own an organic smartphone',
+//       author: 'Karolin',
+//       likes: 162
+//     },
+//     {
+//       title: 'Confusion',
+//       body: 'Friends, why do you just respond with the word `dislike`? Surely you mean to click the like button?',
+//           author: 'Karolin',
+//       likes: -100
+//     },
+//     {
+//       title: 'Vespa',
+//       body: 'Friends, my Vespa has been upgraded to run on old french fry oil. Its top speed is now 11 mph',
+//       author: 'Karolin',
+//       likes: 2
+//     },
+//     {
+//       title: 'Licensed',
+//       body: 'Friends, I am now officially licensed to teach yogalates. Like this to get 10% off a private lesson',
+//       author: 'Karolin',
+//       likes: 3
+//     },
+//     {
+//       title: 'Water',
+//       body: 'Friends, I have been collecting rain water so I can indulge in locally sourced raw water. Ask me how',
+//       author: 'Karolin',
+//     },
+//   ];
 
-  Tweet.insertMany(manyTweets, (error, tweets) =>{
-      if(error){
-          console.log(error)
-      } else {
-          console.log(tweets);
-      } db.close()
-  });
+//   Tweet.insertMany(manyTweets, (error, tweets) =>{
+//       if(error){
+//           console.log(error)
+//       } else {
+//           console.log(tweets);
+//       } db.close()
+//   });
 
-//   Let's find some tweets!
+//   Let's find some tweets! This code will find all the tweets
 
-Tweet.find((error, tweets) => {
+// Tweet.find((error, tweets) => {
+//     console.log(tweets)
+//     db.close()
+// })
+
+// Let's find specific tweet
+
+Tweet.find({title: 'Water'}, (error, tweets) => {
     console.log(tweets)
     db.close()
 })
